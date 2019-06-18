d9692cb5be02ad7d7e7c6b1cbf8e2c701d8a93e9
Maik Rantetasik <maikgibranr@gmail.com>
2019-06-18 17:46:46 +0700

    Add initial explore scene

---
+++ Assets/Scripts/Events/BackgroundScroll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Events {
+    public class BackgroundScroll : MonoBehaviour {
+        [SerializeField]
+        private RectTransform bgTransform;
+        [SerializeField]
+        private float speed;
+        [SerializeField]
+        private float overlapXPos;
+
+        private Vector2 initialPos;
+
+        private void Awake() {
+            initialPos = bgTransform.anchoredPosition;
+        }
+
+        private void Update() {
+            float newXPos = bgTransform.anchoredPosition.x - (speed * Time.deltaTime);
+            bgTransform.anchoredPosition = new Vector2(newXPos, bgTransform.anchoredPosition.y);
+            if (newXPos < overlapXPos) {
+                bgTransform.anchoredPosition = initialPos;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ExploreSystemController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class ExploreSystemController : MonoBehaviour {
+        [SerializeField]
+        PlayerController playerControl;
+
+        void Start () {
+            playerControl.ExploreMode(true);
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+    }
+}
